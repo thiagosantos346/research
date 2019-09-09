4edd48623716e0e68b7348bd3089db22092503f6
thiagosantos346 <thiagosantos346@gmail.com>
2019-09-09 18:46:08 -0300

    implementação com arquivos

--- JaccardIndex/src/com/jaccard/main/Main.java
+++ JaccardIndex/src/com/jaccard/main/Main.java
@@ -1,19 +1,50 @@
 package com.jaccard.main;
 
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
+	private static Jaccard myset = new Jaccard();;
 	
-	public static void main(String[] args) {
-		
-		Jaccard myset;
-		myset = new Jaccard();
+	public static void showAllSimilarity(){
 		Float  res;
-		myset.add("thiago");
-		myset.add("theago");
-		myset.add("abfg");
-		myset.add("cdfg");
-		res = myset.jaccardValue(2,3);
-		System.out.println( " Similaridade : "+res );
+		
+		
+		
+		for(int i = 0; i < myset.set.size(); i++ ){
+			for(int j = i; j < myset.set.size() - 1; j++ ){
+				res = myset.jaccardValue(i,j);
+				System.out.println( "Similaridade : "+res );
+				System.out.println( myset.set.get(i).value+" : "+myset.set.get(j).value+"\n" );
+				
+			}	
+		}
+		
+		
+
+	}
+	
+	public static void fileReader(String fileName) throws IOException {
+		InputStream inStream = new FileInputStream(fileName);
+		InputStreamReader reStream = new InputStreamReader(inStream);
+		BufferedReader buffer = new BufferedReader(reStream);
+		String str;	
+		do{
+		
+			str = buffer.readLine();
+			if(str !=  null)
+			myset.add(str);
+		
+		}while(str != null);
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		fileReader("/home/silvathiago/git/research/JaccardIndex/src/com/jaccard/main/wordlist.txt");
+		showAllSimilarity();
 
 	}
 
